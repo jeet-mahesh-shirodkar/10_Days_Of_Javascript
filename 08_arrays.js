83f1c0fee3398777e9e1ccd02ea1a01016c72f8c
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-11 02:29:44 +0530

    array

---
+++ 08_arrays.js
@@ -0,0 +1,21 @@
+/**
+ * Task
+
+Complete the getSecondLargest function in the editor below. 
+It has one parameter: an array,nums, of n numbers. 
+The function must find and return the second largest number in numbs.
+
+Input Format
+
+Locked stub code in the editor reads the following input from stdin and passes it to the function:
+The first line contains an integer,n , denoting the size of the nums array.
+The second line contains n space-separated numbers describing the elements in nums.
+ */
+
+ let nums=[1,2,3,6,6,5];
+function getSecondLargest(nums) {
+    // Complete the function
+    for(i=0;i<=nums.length;i++){
+        nums[i];
+    }
+}
