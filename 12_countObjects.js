714ebd6b315b8896adebac5118c89d78af995323
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-20 01:26:04 +0530

    get Count Object

---
+++ 12_countObjects.js
@@ -0,0 +1,17 @@
+/**
+ * Task
+ * Complete the function in the editor. 
+ * It has one parameter: an array, a, of objects.
+ * Each object in the array has two integer properties denoted by x and y. 
+ * The function must return a count of all such objects  o in array  that satisfy o.x = 0.y.
+ */
+
+var objects =[{x:4,y:4},{x:4,y:2},{x:5,y:5},{x:3,y:2}];
+
+function getCount(objects){
+  var count = 0;
+
+  return count;
+}
+
+console.log(getCount(objects));
