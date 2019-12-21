5cc9a105d2dc403e268eb49c196e7c61fd723ce1
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-21 18:02:14 +0530

    Classes

--- 13_01_classes.js
+++ 13_01_classes.js
@@ -21,6 +21,7 @@
   * We can use this getInformation() method internally also
   * The drawback of internally defining the getInformation function is that, 
   * It recreates that function every time we create a new Fruit object.
+  
   The Prototype Property
   * Fortunately, every function in JavaScript has something called a prototype property.
     Which is empty by default. We can think of a function's prototype as an object blueprint or paradigm.
@@ -41,3 +42,61 @@ let lime = new fruits("Lime");
 lime.color = "Green";
 
 console.log(lime.getInformation);
+
+/**
+ * Classes
+ * JavaScript classes, introduced in ECMAScript 6, are essentially syntactic sugar over JavaScript's existing
+   prototype-based inheritance that don't actually introduce a new object-oriented inheritance model.
+   This syntax is a means of more simply and clearly creating objects and deal with inheritance.
+
+   Class Declarations
+ * One way to define a class is using a class declaration. 
+   To declare a class, we use the class keyword and follow it with the class' name. 
+   Ideally, we always write class names in TitleCase.
+ */
+
+class Polygon {
+  constructor(height, width) {
+      this.height = height;
+      this.width = width;
+  }
+} 
+
+let p = new Polygon(1,2);
+console.log('Polygon p:',p);
+
+/**An important difference between function declarations and class declarations is that function declarations are
+ *  Hoisted (i.e., can be referenced before they're declared) but class declarations are not.
+ *  This means we must first declare our class before attempting to access (or reference) it.
+ *  If we fail to do so, our code throws a ReferenceError.
+ * */
+
+try {
+  let p = new Polygon(1, 2);
+  console.log('Polygon p:', p);
+}
+catch (exception) {
+  console.log(exception.name + ': ' + exception.message);
+}
+class Polygon {
+  constructor(height, width) {
+      this.height = height;
+      this.width = width;
+  }
+}
+
+p = new Polygon(1, 2);
+console.log('Polygon p:', p);
+
+/**
+ * 
+ */
+
+
+
+
+
+
+
+
+
---
+++ 13_classes.js
@@ -0,0 +1,15 @@
+/**
+ * Task
+ * Create a Polygon class that has the following properties:
+ * A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+ * A perimeter() method that returns the polygon's perimeter.
+ * Locked code in the editor tests the Polygon constructor and the perimeter method.
+Note: The perimeter method must be lowercase and spelled 
+ */
+
+ class Polygon{
+
+    Polygon(sides){
+       this.sides = [];
+    }
+ }
