eb2edbd3bff16bd7ec1fd1f69fbf5b2fa887b6f5
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-22 15:15:36 +0530

    Classes

--- 13_01_classes.js
+++ 13_01_classes.js
@@ -89,8 +89,86 @@ p = new Polygon(1, 2);
 console.log('Polygon p:', p);
 
 /**
- * 
+ * Class Expressions
+ * Class expressions are another way to define a class, and they can be either named or unnamed. 
+ * The name given to a named class expression is local to the class' body.
  */
+
+let Polygon = class {
+  constructor(height, width) {
+      this.height = height;
+      this.width = width;
+  }
+};
+
+console.log('Polygon:', Polygon);
+let p = new Polygon(1, 2);
+console.log('p:', p);
+
+/**
+ * The Constructor Method
+ * The constructor method is a special method we use to create and initialize objects of a class.
+   A class can only have one special method with the name constructor, 
+   and attempting to write a class containing more than one constructor method will throw a SyntaxError.
+   To implement inheritance, we can use the super keyword in a constructor to call a parent class constructor.
+ */
+
+'use strict';
+
+class Polygon {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    getArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Polygon(10, 10);
+
+console.log(square.getArea());
+
+/**
+ * Static Methods
+ * Static methods are methods relevant to all instances of a class — not just any one instance. 
+ * These methods receive information from their arguments and not a class instance, 
+ * which allows us to invoke a class' static methods without creating an instance of the class. 
+ * In fact, we actually can't call a static method on an instantiated class object (attempting to do so throws a TypeError).
+ * We define a class' static methods using the static keyword.
+ * We typically use these methods to create utility functions for applications, as they can't be called on class objects.
+ */
+
+'use strict';
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+// The correct way to call a static method
+console.log(Point.distance(p1, p2));
+
+// Attempt to call a static method on an instance of the class
+try {
+    console.log(p1.distance(p1, p2));
+}
+catch (exception) {
+    console.log(exception.name + ': ' + exception.message);
+}
+
+
+
 
 
 
--- 13_classes.js
+++ 13_classes.js
@@ -7,9 +7,18 @@
 Note: The perimeter method must be lowercase and spelled 
  */
 
- class Polygon{
 
-    Polygon(sides){
-       this.sides = [];
-    }
- }
+ class Polygon {
+   constructor(sides) {
+       this.sides = sides;
+   }
+   perimeter() {
+       var sum = 0;
+       for(var i=0;i< this.sides.length; i++){
+           sum = sum + this.sides[i];
+       }
+       return sum;
+   }
+}
+
+
