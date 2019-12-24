7bf04a832181f09c413a72d5771f3613edf811cb
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-24 22:27:24 +0530

    Template Liternal

---
+++ 15_templateLiteral.js
@@ -0,0 +1,11 @@
+/**
+ * Task
+ * The code in the editor has a tagged template literal that passes the area and perimeter of a rectangle to a tag function named sides. Recall that the first argument of a tag function is an array of string literals from the template, and the subsequent values are the template's respective expression values.
+ * Complete the function in the editor so that it does the following:
+ * Finds the initial values of  and  by plugging the area and perimeter values into the formula:
+ * where  is the rectangle's area and  is its perimeter.
+ * Creates an array consisting of  and  and sorts it in ascending order.
+ * Returns the sorted array
+ */
+
+ 
