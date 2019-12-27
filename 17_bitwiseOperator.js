519a306ef1095e9414a87e5702301a50aac1834a
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-27 23:49:23 +0530

    Bitwise Operator

---
+++ 17_bitwiseOperator.js
@@ -0,0 +1,7 @@
+/**
+ * Task
+ * We define  to be a sequence of distinct sequential integers from  to ; in other words, . 
+ * We want to know the maximum bitwise AND value of any two integers,  and  (where ), in sequence  that is also less than a given integer, .
+ * Complete the function in the editor so that given  and , it returns the maximum .
+ * Note: The  symbol represents the bitwise AND operator.
+ */
