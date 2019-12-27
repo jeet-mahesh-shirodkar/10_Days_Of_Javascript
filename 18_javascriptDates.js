d21b80aadea312e7a9c19c6f48ad7061b685f56a
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2019-12-28 01:27:56 +0530

    JS Date

---
+++ 18_javascriptDates.js
@@ -0,0 +1,7 @@
+/**
+ * Task
+ * Given a date string, dateString, in the format MM/DD/YYYY, find and return the day name for that date. 
+ * Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. 
+ * For example, the day name for the date 12/07/2016 is Wednesday. 
+ * */
+
